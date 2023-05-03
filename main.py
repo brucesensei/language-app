56a1112ea1dbcb244f9f922f33ba6e3760ca3f40
nolan bruce <nolan_76@msn.com>
2023-05-03 11:54:10 +0900

    inital commit

---
+++ main.py
@@ -0,0 +1,114 @@
+import io
+import json
+import sys
+import pyperclip as pc
+from time import sleep
+
+def get_data(file_name):
+  """retrieve JSON object and return a python dictionary"""
+  with io.open(file_name,'r',encoding='utf8') as file:
+    data = file.read()
+    data = json.loads(data)
+    return data 
+
+def create_dict(new_data):
+  """create a dictionary of the data entries"""
+  i = 0
+  new_dict = {}
+  while i < len(new_data):
+    new_dict[new_data[i]] =  new_data[i + 1]
+    i += 2
+  return new_dict
+
+def add_lesson():
+  null_value = input('Copy data to the clipboard and press enter.')
+  new_entry = pc.paste()
+  with io.open('new_data.txt','w',encoding='utf8') as f:
+      f.write(new_entry)
+  with io.open('new_data.txt','r',encoding='utf8') as f:
+      data = f.read()
+  data = data.split('\n')
+  new_data = [i for i in data if i != '']
+  new_dict = create_dict(new_data)
+  lesson_data = get_data('learning.json')
+  title = input('Enter the unit title: \n')
+  lesson_data[title] = new_dict
+  with io.open('learning.json','w',encoding='utf8') as outfile:
+      json.dump(lesson_data, outfile)
+
+def display_lessons(display_list):
+  """print the list of learning modules"""
+  print('''
+SPANISH LANGUAGE LESSONS
+========================
+''')
+  for count, list_item in enumerate(display_list, start=1):
+    print(count, list_item.upper())
+
+def display_options():
+  print('''
+ ________________________________________________
+|                                                |
+| - To review a lesson, enter the lesson number. |
+| - To add a lesson, enter "a".                  |
+| - To remove a lesson, enter "r".               |
+| - To quit, press "q".                          |
+|________________________________________________|
+        ''')
+
+def get_user_choice(display_list):
+  """Get the choice from the user"""
+  while True:
+    valid_choices = [str(i+1) for i in range(len(display_list))]
+    valid_choices.extend(['a', 'r', 'q'])
+    user_choice = input('Choose one. ')
+    if user_choice not in valid_choices:
+      continue
+    return user_choice
+
+def display_learning_module(user_choice, display_list, spanish_dict):
+  """Display the learning module title and justified list of vocabulary"""
+  title = display_list[int(user_choice) - 1]
+  print(title.upper())
+  print('=' * (len(title)) +  '\n')
+  for k, v in spanish_dict[title].items():
+    print(k + ' ' + '.' * (80 - len(k) - len(v)) + ' ' + v + '\n')
+
+def remove_lesson(display_list):
+  user_choice = get_user_choice(display_list)
+  spanish_dict = get_data('learning.json')
+  display_list = list(spanish_dict.keys())
+  to_remove = display_list[int(user_choice) - 1]
+  del spanish_dict[to_remove]
+  with open("learning.json", "w") as outfile:
+    json.dump(spanish_dict, outfile)
+  print(f"{to_remove} has been removed.")
+
+def main():
+  while True:
+    spanish_dict = get_data('learning.json')
+    display_list = list(spanish_dict.keys())
+    display_lessons(display_list)
+    display_options()
+    user_choice = get_user_choice(display_list)
+    if user_choice == 'r':
+      remove_lesson(display_list)
+      continue
+    if user_choice == 'a':
+      add_lesson()
+      continue
+    if user_choice == 'q':
+      print('You are now leaving the land of learning.')
+      sleep(2)
+      sys.exit()
+    display_learning_module(user_choice, display_list, spanish_dict)
+    review = input('To go back press "g". Otherwise, press any other key to quit ')
+    if review == 'g':
+      continue
+    print('You are now leaving the land of learning.')
+    sleep(2)
+    sys.exit()
+
+  
+if __name__ == '__main__':
+  main()
